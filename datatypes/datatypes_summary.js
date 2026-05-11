a13f1d4a31d831fa9ff6575f5ba9cbeb9aab4ce8
22Raghava2004 <raghavangowda2004@gmail.com>
2026-05-12 00:04:45 +0530

    first js

--- datatypes/datatypes_summary.js
+++ datatypes/datatypes_summary.js
@@ -48,3 +48,45 @@ const myfuc=function (){
 
 console.log(typeof myfuc)
 
+///++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+//stack (primitive) ,heap (non primitive)
+
+// in stack the primitive the data will be copied and send forward to the other variable if requested
+
+// in heap the non primitivethe reference will be send forward if rhe data in one changes theentire data will change in the other data
+
+
+let username='raghava'
+
+let reusername=username
+
+reusername='ragupati raghava'
+
+console.log(username)
+console.log(reusername)
+
+//this output show the name will not be changed because the data is stored in stack and the data wont be updated if the variable namechanges
+
+
+
+let maga ={
+    email:'goga@gmail.com',
+    password:'raghav1234',
+}
+
+let remaga=maga
+
+remaga.password='#Ragha2005'
+
+console.log(maga)
+console.log(remaga)
+
+let array=['super man','batman','wonder women','spooder man']
+
+let rearray=array
+
+rearray.push('flash')
+console.log(array)
+console.log(rearray)
